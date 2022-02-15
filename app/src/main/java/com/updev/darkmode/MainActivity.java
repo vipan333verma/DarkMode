41c6f962ae443266983e95d052506f8279578fd9
vipan333verma <31509400+vipan333verma@users.noreply.github.com>
2022-02-16 03:48:04 +0530

    Initial commit

---
+++ app/src/main/java/com/updev/darkmode/MainActivity.java
@@ -0,0 +1,106 @@
+package com.updev.darkmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initIDs();
+    }
+
+    private void initIDs() {
+
+        button =findViewById(R.id.button);
+
+        SharedPreferences sharedPreferences
+                = getSharedPreferences(
+                "sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor
+                = sharedPreferences.edit();
+        final boolean isDarkModeOn
+                = sharedPreferences
+                .getBoolean(
+                        "isDarkModeOn", false);
+
+        // When user reopens the app
+        // after applying dark/light mode
+        if (isDarkModeOn) {
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_YES);
+            button.setText(
+                    "Disable Dark Mode");
+        }
+        else {
+            AppCompatDelegate
+                    .setDefaultNightMode(
+                            AppCompatDelegate
+                                    .MODE_NIGHT_NO);
+            button
+                    .setText(
+                            "Enable Dark Mode");
+        }
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view)
+                    {
+                        // When user taps the enable/disable
+                        // dark mode button
+                        if (isDarkModeOn) {
+
+                            // if dark mode is on it
+                            // will turn it off
+                            AppCompatDelegate
+                                    .setDefaultNightMode(
+                                            AppCompatDelegate
+                                                    .MODE_NIGHT_NO);
+                            // it will set isDarkModeOn
+                            // boolean to false
+                            editor.putBoolean(
+                                    "isDarkModeOn", false);
+                            editor.apply();
+
+                            // change text of Button
+                            button.setText(
+                                    "Enable Dark Mode");
+                        }
+                        else {
+
+                            // if dark mode is off
+                            // it will turn it on
+                            AppCompatDelegate
+                                    .setDefaultNightMode(
+                                            AppCompatDelegate
+                                                    .MODE_NIGHT_YES);
+
+                            // it will set isDarkModeOn
+                            // boolean to true
+                            editor.putBoolean(
+                                    "isDarkModeOn", true);
+                            editor.apply();
+
+                            // change text of Button
+                            button.setText(
+                                    "Disable Dark Mode");
+                        }
+                    }
+                });
+
+    }
+}
